129e24c61c7b33b3fd910b6ad039ebeffe7cfcc7
Denis <dingo_bastard@hotmail.com>
2016-09-25 09:49:51 +0200

    Version update 1.1.1 - Minor changes - selection view saved to variable, added docstrings to definitions module.

--- css_toc.py
+++ css_toc.py
@@ -1,81 +1,98 @@
 import sublime
 import sublime_plugin
 
-import math
 from .css_toc_definitions import *
 
-# Section creation command
+""" Definition inside classes:
+
+selection - Text selection at the cursor position.
+pos - The position of the beginning of the selection.
+contents - Contents of the file up to the position of the cursor.
+region - The region of the selected line/text.
+selected - The selected text.
+line - Line contents where the cursor is.
+
+"""
+
 class CsstocCreateSectionCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		pos = self.view.sel()[0].begin() # Cursors position
-		contents = self.view.substr(sublime.Region(0, pos)) # Contents of the file up to the position of the cursor
-		region = self.view.line(self.view.sel()[0]) # Cursor region
-		selected = self.view.substr(sublime.Region(self.view.sel()[0].begin(), self.view.sel()[0].end())) # Selected text
-		line = self.view.substr(self.view.line(self.view.sel()[0])) # Returns the line contents of the cursor
+    """Create section command."""
 
-		text = get_section(contents, selected, line)
-		self.view.replace(edit, region, text)
+    def run(self, edit):
+        selection = self.view.sel()[0]
+        pos = selection.begin()
+        contents = self.view.substr(sublime.Region(0, pos))
+        region = self.view.line(selection)
+        selected = self.view.substr(sublime.Region(pos, selection.end()))
+        line = self.view.substr(self.view.line(selection))
 
-		# Select only text
-		text_region = self.view.find('([^\d\W]+)', region.begin())
+        text = get_section(contents, selected, line)
+        self.view.replace(edit, region, text)
 
-		self.view.sel().clear()
-		self.view.sel().add(text_region)
-		self.view.show_at_center(text_region)
+        # Select only text
+        text_region = self.view.find(r'([^\d\W]+)', region.begin())
+
+        self.view.sel().clear()
+        self.view.sel().add(text_region)
+        self.view.show_at_center(text_region)
 
 
-# Subsection creation command
 class CsstocCreateSubsectionCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		pos = self.view.sel()[0].begin() # Cursors position
-		contents = self.view.substr(sublime.Region(0, pos)) # Contents of the file up to the position of the cursor
-		region = self.view.line(self.view.sel()[0]) # Cursor region
-		selected = self.view.substr(sublime.Region(self.view.sel()[0].begin(), self.view.sel()[0].end())) # Selected text
-		line = self.view.substr(self.view.line(self.view.sel()[0])) # Returns the line contents of the cursor
+    """Create sub section command."""
 
-		text = get_subsection(contents, selected, line)
-		self.view.replace(edit, region, text)
+    def run(self, edit):
+        selection = self.view.sel()[0]
+        pos = selection.begin()
+        contents = self.view.substr(sublime.Region(0, pos))
+        region = self.view.line(selection)
+        selected = self.view.substr(sublime.Region(pos, selection.end()))
+        line = self.view.substr(self.view.line(selection))
 
-		# Select only text
-		text_region = self.view.find('([^\d\W]+)', region.begin())
+        text = get_subsection(contents, selected, line)
+        self.view.replace(edit, region, text)
 
-		self.view.sel().clear()
-		self.view.sel().add(text_region)
-		self.view.show_at_center(text_region)
+        # Select only text
+        text_region = self.view.find(r'([^\d\W]+)', region.begin())
+
+        self.view.sel().clear()
+        self.view.sel().add(text_region)
+        self.view.show_at_center(text_region)
 
 
-# Subsubsection creation command
 class CsstocCreateSubsubsectionCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		pos = self.view.sel()[0].begin() # Cursors position
-		contents = self.view.substr(sublime.Region(0, pos)) # Contents of the file up to the position of the cursor
-		region = self.view.line(self.view.sel()[0]) # Cursor region
-		selected = self.view.substr(sublime.Region(self.view.sel()[0].begin(), self.view.sel()[0].end())) # Selected text
-		line = self.view.substr(self.view.line(self.view.sel()[0])) # Returns the line contents of the cursor
+    """Create sub sub section command."""
 
-		text = get_subsubsection(contents, selected, line)
-		self.view.replace(edit, region, text)
+    def run(self, edit):
+        selection = self.view.sel()[0]
+        pos = selection.begin()
+        contents = self.view.substr(sublime.Region(0, pos))
+        region = self.view.line(selection)
+        selected = self.view.substr(sublime.Region(pos, selection.end()))
+        line = self.view.substr(self.view.line(selection))
 
-		# Select only text
-		text_region = self.view.find('([^\d\W]+)', region.begin())
+        text = get_subsubsection(contents, selected, line)
+        self.view.replace(edit, region, text)
 
-		self.view.sel().clear()
-		self.view.sel().add(text_region)
-		self.view.show_at_center(text_region)
+        # Select only text
+        text_region = self.view.find(r'([^\d\W]+)', region.begin())
+
+        self.view.sel().clear()
+        self.view.sel().add(text_region)
+        self.view.show_at_center(text_region)
 
 
-# Create Table of content command
 class CsstocCreateTocCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		contents = self.view.substr(sublime.Region(0, self.view.size())) # Contents of the file
-		existing_toc_pat = '^(\/\*[\s\w\n\d\[\].]+\*\/)' # Pattern for the existing TOC
-		toc_exists = re.findall(existing_toc_pat, contents, re.MULTILINE) # Search of the exisitng TOC
-		toc_region = self.view.find(existing_toc_pat, 0) # Region of the existing TOC
+    """Create table of contents command."""
 
-		toc = get_toc(contents)
+    def run(self, edit):
+        contents = self.view.substr(sublime.Region(0, self.view.size())) # Contents of the file
+        existing_toc_pat = r'^(\/\*[\s\w\n\d\[\].]+\*\/)' # Pattern for the existing TOC
+        toc_exists = re.findall(existing_toc_pat, contents, re.MULTILINE) # Search of the exisitng TOC
+        toc_region = self.view.find(existing_toc_pat, 0) # Region of the existing TOC
 
-		if not toc_exists:
-			self.view.insert(edit, 0, toc)
-		else:
-			self.view.replace(edit, toc_region, '')
-			self.view.insert(edit, toc_region.begin(), toc)
+        toc = get_toc(contents)
+
+        if not toc_exists:
+            self.view.insert(edit, 0, toc)
+        else:
+            self.view.replace(edit, toc_region, '')
+            self.view.insert(edit, toc_region.begin(), toc)
--- css_toc_definitions.py
+++ css_toc_definitions.py
@@ -1,9 +1,11 @@
-#
-# Definitions for the "CSS Table of Contents" plugin for ST3
-#
+"""Definitions for the "CSS Table of Contents" plugin for ST3
+
+This module contains all the definitions used in the main file for the
+CSS Table of Contents plugin.
+
+"""
 
 import re
-import heapq
 from itertools import chain
 
 # Pattern constants
@@ -13,12 +15,27 @@ SUB_SECTION_PREFIX = '/*****---------- '
 SUB_SECTION_SUFFIX = ' ----------*****/'
 SUB_SUB_SECTION_PREFIX = '/**------------- '
 SUB_SUB_SECTION_SUFFIX = ' -------------**/'
-SECTION_PATTERN = '^(\/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\* \d+)'
-SUB_SECTION_PATTERN = '^(\/\*\*\*\*\*\-\-\-\-\-\-\-\-\-\- \d+\.\d+)'
-SUB_SUB_SECTION_PATTERN = '^(\/\*\*\-\-\-\-\-\-\-\-\-\-\-\-\- \d+\.\d+\.\d+)'
+SECTION_PATTERN = r'^(\/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\* \d+)'
+SUB_SECTION_PATTERN = r'^(\/\*\*\*\*\*\-\-\-\-\-\-\-\-\-\- \d+\.\d+)'
+SUB_SUB_SECTION_PATTERN = r'^(\/\*\*\-\-\-\-\-\-\-\-\-\-\-\-\- \d+\.\d+\.\d+)'
+
 
 def get_section(contents, selected, line):
+    """ Function that returns the section.
 
+    The function takes contents above the pointer, looks at the selection or line,
+    and create a secion based on the contents (if there is a section or not), on the
+    line.
+
+    Args:
+        contents (str): The contents above the pointer.
+        selected (str): Selected text to be the name of the section.
+        line (str): The line where the pointer is.
+
+    Returns:
+        str: The section string with prefixes and suffixes and correct
+             section number.
+    """
     if len(selected) == 0:
         if len(line) == 0:
             text = 'Section'
@@ -31,7 +48,7 @@ def get_section(contents, selected, line):
     section = re.findall(SECTION_PATTERN, contents, re.MULTILINE|re.DOTALL)
 
     if section:
-        section_no = int(re.findall('(\d+)',section[len(section)-1])[0])
+        section_no = int(re.findall(r'(\d+)', section[len(section)-1])[0])
     else:
         section_no = 0
 
@@ -41,7 +58,21 @@ def get_section(contents, selected, line):
 
 
 def get_subsection(contents, selected, line):
+    """ Function that returns the sub section.
 
+    The function takes contents above the pointer, looks at the selection or line,
+    and create a sub secion based on the contents (if there is a sub section or not), on the
+    line.
+
+    Args:
+        contents (str): The contents above the pointer.
+        selected (str): Selected text to be the name of the sub section.
+        line (str): The line where the pointer is.
+
+    Returns:
+        str: The sub section string with prefixes and suffixes and correct
+             sub section number.
+    """
     if len(selected) == 0:
         if len(line) == 0:
             text = 'Subsection'
@@ -54,10 +85,10 @@ def get_subsection(contents, selected, line):
     section = re.findall(SECTION_PATTERN, contents, re.MULTILINE|re.DOTALL)
 
     if section:
-        section_no = int(re.findall('(\d+)',section[len(section)-1])[0])
+        section_no = int(re.findall(r'(\d+)', section[len(section)-1])[0])
         subsection = re.findall(SUB_SECTION_PATTERN, contents, re.MULTILINE|re.DOTALL)
         if subsection:
-            subsection_nu = re.findall('(\d+)\.(\d+)',subsection[len(subsection)-1],re.DOTALL)[0]
+            subsection_nu = re.findall(r'(\d+)\.(\d+)', subsection[len(subsection)-1], re.DOTALL)[0]
             if int(subsection_nu[0]) < section_no:
                 subsection_no = 0
             else:
@@ -73,7 +104,21 @@ def get_subsection(contents, selected, line):
 
 
 def get_subsubsection(contents, selected, line):
+    """ Function that returns the sub sub section.
 
+    The function takes contents above the pointer, looks at the selection or line,
+    and create a sub sub section based on the contents (if there is a sub sub section or not),
+    on the line.
+
+    Args:
+        contents (str): The contents above the pointer.
+        selected (str): Selected text to be the name of the sub sub section.
+        line (str): The line where the pointer is.
+
+    Returns:
+        str: The section string with prefixes and suffixes and correct
+             sub sub section number.
+    """
     if len(selected) == 0:
         if len(line) == 0:
             text = 'Subsubsection'
@@ -90,9 +135,9 @@ def get_subsubsection(contents, selected, line):
         subsubsection = re.findall(SUB_SUB_SECTION_PATTERN, contents, re.MULTILINE|re.DOTALL)
 
         if subsubsection:
-            section_no = int(re.findall('(\d+)',section[len(section)-1])[0])
-            subsection_no = int(re.findall('(\d+)\.(\d+)',subsection[len(subsection)-1],re.DOTALL)[0][1])
-            subsubsection_nu = re.findall('(\d+)\.(\d+)\.(\d+)',subsubsection[len(subsubsection)-1],re.DOTALL)[0]
+            section_no = int(re.findall(r'(\d+)', section[len(section)-1])[0])
+            subsection_no = int(re.findall(r'(\d+)\.(\d+)', subsection[len(subsection)-1], re.DOTALL)[0][1])
+            subsubsection_nu = re.findall(r'(\d+)\.(\d+)\.(\d+)', subsubsection[len(subsubsection)-1], re.DOTALL)[0]
             if int(subsubsection_nu[0]) < section_no:
                 subsubsection_no = 0
             else:
@@ -101,8 +146,8 @@ def get_subsubsection(contents, selected, line):
                 else:
                     subsubsection_no = int(subsubsection_nu[2])
         else:
-            section_no = int(re.findall('(\d+)',section[len(section)-1])[0])
-            subsection_no = int(re.findall('(\d+)\.(\d+)',subsection[len(subsection)-1],re.DOTALL)[0][1])
+            section_no = int(re.findall(r'(\d+)', section[len(section)-1])[0])
+            subsection_no = int(re.findall(r'(\d+)\.(\d+)', subsection[len(subsection)-1], re.DOTALL)[0][1])
             subsubsection_no = 0
 
         subsubsection_no += 1
@@ -113,6 +158,18 @@ def get_subsubsection(contents, selected, line):
 
 
 def clean_section(contents):
+    """ Function that returns the list with the section name
+    stripped from the prefixes and suffixes.
+
+    The function takes contents above the pointer, finds the section pattern
+    and replaces the pattern prefixes and suffixes from it, and appends it to a list.
+
+    Args:
+        contents (str): The contents above the pointer.
+
+    Returns:
+        list: List with all the sections sorted.
+    """
     section_pat = r'^(\/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*.+)$'
     all_sections = re.findall(section_pat, contents, re.MULTILINE)
     sections = []
@@ -126,7 +183,18 @@ def clean_section(contents):
 
 
 def clean_subsection(contents):
+    """ Function that returns the list with the sub section name
+    stripped from the prefixes and suffixes.
 
+    The function takes contents above the pointer, finds the sub section pattern
+    and replaces the pattern prefixes and suffixes from it, and appends it to a list.
+
+    Args:
+        contents (str): The contents above the pointer.
+
+    Returns:
+        list: List with all the sub sections sorted.
+    """
     subsection_pat = r'^(\/\*\*\*\*\*\-\-\-\-\-\-\-\-\-\-.+)$'
     all_subsections = re.findall(subsection_pat, contents, re.MULTILINE)
     subsections = []
@@ -140,7 +208,18 @@ def clean_subsection(contents):
 
 
 def clean_subsubsection(contents):
+    """ Function that returns the list with the sub sub section name
+    stripped from the prefixes and suffixes.
 
+    The function takes contents above the pointer, finds the sub sub section pattern
+    and replaces the pattern prefixes and suffixes from it, and appends it to a list.
+
+    Args:
+        contents (str): The contents above the pointer.
+
+    Returns:
+        list: List with all the sub sub sections sorted.
+    """
     subsubsection_pat = r'^(\/\*\*\-\-\-\-\-\-\-\-\-\-\-\-\-.+)$'
     all_subsubsections = re.findall(subsubsection_pat, contents, re.MULTILINE)
     subsubsections = []
@@ -154,20 +233,31 @@ def clean_subsubsection(contents):
 
 
 def get_toc(contents):
+    """ Function that returns the table of contents.
 
+    The function takes all the contents in the file and returns the
+    ready and indented table of contents.
+
+    Args:
+        contents (str): The contents in the file.
+
+    Returns:
+        str: The table of contents to be placed at the top of the .css file.
+    """
     sections = clean_section(contents)
     subsections = clean_subsection(contents)
     subsubsections = clean_subsubsection(contents)
 
-    sorted_list = list(sorted(chain.from_iterable((sections, subsections, subsubsections)), key = lambda x: [int(i) for i in re.findall(r'\d+', x)]))
+    sorted_list = list(sorted(chain.from_iterable((sections, subsections, subsubsections)),
+                              key=lambda x: [int(i) for i in re.findall(r'\d+', x)]))
 
     toc = ''
 
     for tocitem in sorted_list:
 
-        if re.match('(\d+)\.(\d+)\.(\d+)', tocitem):
+        if re.match(r'(\d+)\.(\d+)\.(\d+)', tocitem):
             toc += '\t\t' + tocitem + '\n'
-        elif re.match('(\d+)\.(\d+)', tocitem):
+        elif re.match(r'(\d+)\.(\d+)', tocitem):
             toc += '\t' + tocitem + '\n'
         else:
             toc += tocitem + '\n'
